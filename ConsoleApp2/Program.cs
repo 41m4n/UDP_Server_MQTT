81429ef2b034d3b5d7f01c4a8043f1f3a3efdd36
NEWICT\aiman <aiman@newict.com>
2019-09-24 10:04:43 +0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static UdpClient udpClient = new UdpClient();
+        static IPEndPoint from;
+        static void Main(string[] args)
+        {
+            //=============================================================UDPER
+            //UDPer udp = new UDPer();
+            //udp.Start();
+
+            //ConsoleKeyInfo cki;
+            //do
+            //{
+            //    if (Console.KeyAvailable)
+            //    {
+            //        cki = Console.ReadKey(true);
+            //        switch (cki.KeyChar)
+            //        {
+            //            case 's':
+            //                udp.Send(new Random().Next().ToString());
+            //                break;
+            //            case 'x':
+            //                udp.Stop();
+            //                return;
+            //        }
+            //    }
+            //    Thread.Sleep(10);
+            //} while (true);
+            //=============================================================UDPER
+
+
+            //=============================================================UDPSocket
+            //UDPSocket s = new UDPSocket();
+            //s.Server("127.0.0.1", 27000);
+
+            //Console.ReadKey();
+            //=============================================================UDPSocket
+
+            //=============================================================UDPClient
+            int PORT = 27000;
+            //UdpClient udpClient = new UdpClient();
+            udpClient = new UdpClient();
+            udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, PORT));
+
+            from = new IPEndPoint(0, 0);
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    var recvBuffer = udpClient.Receive(ref from);
+                    processMessage(Encoding.UTF8.GetString(recvBuffer), from);
+                    Console.WriteLine(Encoding.UTF8.GetString(recvBuffer));                    
+                }
+            });
+
+            //var data = Encoding.UTF8.GetBytes("ABCD");
+            //udpClient.Send(data, data.Length, from);
+
+
+            //=============================================================UDPClient
+
+            Console.ReadKey();
+
+        }
+
+        public static void processMessage(string message, IPEndPoint targetUdp) {
+            try
+            {
+                if (string.Equals(message, "GetIP"))
+                {
+                    var host = Dns.GetHostEntry(Dns.GetHostName());
+                    Console.WriteLine("Before IP");
+                    foreach (var ips in host.AddressList)
+                    {
+                        Console.WriteLine("In IP");
+                        if (ips.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            Console.WriteLine("In IP2");
+                            message = ips.ToString();
+                        }
+                    }
+                    Console.WriteLine("After IP");
+                    JObject jsonReply = new JObject();
+                    jsonReply.Add("Command", "LocalIP");
+                    jsonReply.Add("ServerIP", message);
+                    var data = Encoding.UTF8.GetBytes(jsonReply.ToString());
+                    udpClient.Send(data, data.Length, targetUdp);
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Error When Processing Message:" + ex.Message);
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/UDPer.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class UDPer
+    {
+        const int PORT_NUMBER = 27000;
+
+        Thread t = null;
+        public void Start()
+        {
+            if (t != null)
+            {
+                throw new Exception("Already started, stop first");
+            }
+            Console.WriteLine("Started listening");
+            StartListening();
+        }
+        public void Stop()
+        {
+            try
+            {
+                udp.Close();
+                Console.WriteLine("Stopped listening");
+            }
+            catch { /* don't care */ }
+        }
+
+        private readonly UdpClient udp = new UdpClient(PORT_NUMBER);
+        IAsyncResult ar_ = null;
+
+        private void StartListening()
+        {
+            ar_ = udp.BeginReceive(Receive, new object());
+        }
+        private void Receive(IAsyncResult ar)
+        {
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, PORT_NUMBER);
+            byte[] bytes = udp.EndReceive(ar, ref ip);
+            string message = Encoding.ASCII.GetString(bytes);
+            Console.WriteLine("From {0} received: {1} ", ip.Address.ToString(), message);
+            if (string.Equals(message, "GetIP"))
+            {
+                Send("Goddamn");
+            }
+            Thread.Sleep(3000);
+            StartListening();
+        }
+        public void Send(string message)
+        {
+            //UdpClient client = new UdpClient();
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse("255.255.255.255"), PORT_NUMBER);
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            Console.WriteLine("Before IP");
+            foreach (var ips in host.AddressList)
+            {
+                Console.WriteLine("In IP");
+                if (ips.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    Console.WriteLine("In IP2");
+                    message = ips.ToString();
+                }
+            }
+            Console.WriteLine("After IP");
+            JObject jsonReply = new JObject();
+            jsonReply.Add("Command", "LocalIP");
+            jsonReply.Add("ServerIP", message);
+
+            byte[] bytes = Encoding.ASCII.GetBytes("Testa");
+            udp.Send(bytes, bytes.Length, ip);
+            //udp.Close();
+            Console.WriteLine("Sent: {0} ", jsonReply.ToString());
+        }
+    }
+}
